61d40c366c4d855d89d1834546b258ba34f84b89
Ильнур Ибрагимов <ilnuribragimov@Mac-mini-Ilnur.local>
2020-06-19 16:50:57 +0200

    login page, regisgter page, react router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import { BrowserRouter as Router } from "react-router-dom";
+import configureRoutes, { history } from "./configureRoutes";
+
+const routes = configureRoutes();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.j</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>{routes}</Router>
     </div>
   );
 }
---
+++ frontend/src/constants/app-routes.js
@@ -0,0 +1,7 @@
+const APP_ROUTES = {
+  home: "/",
+  signIn: "/sign-in",
+  signUp: "/sign-up",
+};
+
+export default APP_ROUTES;
