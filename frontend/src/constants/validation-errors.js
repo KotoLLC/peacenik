cb0434ae3c33b6be4910281782c3923728867879
Matthew Reider <matthew.reider@dynatrace.com>
2020-06-24 16:42:40 +0200

    Revert "login page, regisgter page, react router added"
    This reverts commit 61d40c366c4d855d89d1834546b258ba34f84b89.

--- frontend/src/constants/validation-errors.js
+++
@@ -1,7 +0,0 @@
-export const EMAIL_FORMAT_ERROR = 'Введите правильный имейл';
-export const PHONE_FORMAT_ERROR = 'Неверный формат номера телефона';
-export const REQUIRED_ERROR = 'Поле не может быть пустым';
-export const CONFIRM_PASSWORD_ERROR = 'Пароли не совпадают';
-export const INVALID_VALUE_ERROR = 'Неверное значение';
-export const MIN_VALUE_ERROR = 'Длина поля не может быть меньше %min символов';
-export const INVALID_BIRTHDAY_ERROR = 'Неверный формат даты рождения';
