24bb75fc6959513e1c0312a19c0ac75162e7e7a8
Matthew Reider <matthew.reider@dynatrace.com>
2020-06-28 20:25:16 +0200

    admin is matt

--- test-run.sh
+++ test-run.sh
@@ -6,7 +6,7 @@ trap "kill 0" EXIT
 cd backend
 openssl genrsa -out central.rsa 1024
 go build -o central-service ./central/cmd/
-./central-service -address ":12001" -db central.db -key central.rsa -admin admin@mail.org &
+./central-service -address ":12001" -db central.db -key central.rsa -admin matt@mail.com &
 go build -o node-service ./node/cmd/
 ./node-service -address ":12002" -external "http://localhost:12002" -db node12002.db -central "http://localhost:12001" &
 cd ..
