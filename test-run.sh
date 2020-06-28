b1deb0bce8874747bf7a8538f31c6ebe7651276c
Matthew Reider <matthew.reider@dynatrace.com>
2020-06-28 15:05:16 +0200

    test script in bash

---
+++ test-run.sh
@@ -0,0 +1,17 @@
+# this script is just for testing purposes
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
+cd backend
+openssl genrsa -out central.rsa 1024
+go build -o central-service ./central/cmd/
+./central-service -address ":12001" -db central.db -key central.rsa -admin admin@mail.org &
+go build -o node-service ./node/cmd/
+./node-service -address ":12002" -external "http://localhost:12002" -db node12002.db -central "http://localhost:12001" &
+cd ..
+cd frontend
+npm install
+npm run start &
+
+wait
