9793418daa806fabb9b0303900752ab8b6d929f0
Andrey Sokolov <falconandy@yandex.ru>
2020-07-03 10:08:56 +0400

    Added Comments API

--- backend/node/repo/message_repo.go
+++ backend/node/repo/message_repo.go
@@ -1,13 +1,17 @@
 package repo
 
 import (
+	"database/sql"
 	"errors"
 
 	"github.com/jmoiron/sqlx"
+
+	"github.com/mreider/koto/backend/common"
 )
 
 var (
 	ErrMessageNotFound = errors.New("message not found")
+	ErrCommentNotFound = errors.New("comment not found")
 )
 
 type Message struct {
@@ -19,11 +23,26 @@ type Message struct {
 	UpdatedAt string `json:"updated_at" db:"updated_at"`
 }
 
+type Comment struct {
+	ID        string `json:"id" db:"id"`
+	MessageID string `json:"message_id" db:"message_id"`
+	UserID    string `json:"user_id" db:"user_id"`
+	UserName  string `json:"user_name" db:"user_name"`
+	Text      string `json:"text" db:"text"`
+	CreatedAt string `json:"created_at" db:"created_at"`
+	UpdatedAt string `json:"updated_at" db:"updated_at"`
+}
+
 type MessageRepo interface {
-	AddMessage(message Message) error
 	Messages(userIDs []string) ([]Message, error)
+	Message(messageID string) (Message, error)
+	AddMessage(message Message) error
 	EditMessage(userID, messageID, text, updatedAt string) error
 	DeleteMessage(userID, messageID string) error
+	Comments(messageIDs []string) ([]Comment, error)
+	AddComment(comment Comment) error
+	EditComment(userID, commentID, text, updatedAt string) error
+	DeleteComment(userID, commentID string) error
 }
 
 type messageRepo struct {
@@ -34,18 +53,6 @@ func NewMessages(db *sqlx.DB) MessageRepo {
 	return &messageRepo{
 		db: db,
 	}
-}
-
-func (r *messageRepo) AddMessage(message Message) error {
-	_, err := r.db.Exec(`
-		insert into messages(id, user_id, user_name, text, created_at, updated_at)
-		select $1, $2, $3, $4, $5, $6
-		where not exists(select * from messages where id = $1)`,
-		message.ID, message.UserID, message.UserName, message.Text, message.CreatedAt, message.UpdatedAt)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func (r *messageRepo) Messages(userIDs []string) ([]Message, error) {
@@ -63,6 +70,33 @@ func (r *messageRepo) Messages(userIDs []string) ([]Message, error) {
 		return nil, err
 	}
 	return messages, nil
+}
+
+func (r *messageRepo) Message(messageID string) (Message, error) {
+	var message Message
+	err := r.db.Get(&message, `
+		select id, user_id, user_name, text, created_at, updated_at
+		from messages
+		where id = $1`, messageID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return message, ErrMessageNotFound
+		}
+		return message, err
+	}
+	return message, nil
+}
+
+func (r *messageRepo) AddMessage(message Message) error {
+	_, err := r.db.Exec(`
+		insert into messages(id, user_id, user_name, text, created_at, updated_at)
+		select $1, $2, $3, $4, $5, $6
+		where not exists(select * from messages where id = $1)`,
+		message.ID, message.UserID, message.UserName, message.Text, message.CreatedAt, message.UpdatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *messageRepo) EditMessage(userID, messageID, text, updatedAt string) error {
@@ -85,10 +119,71 @@ func (r *messageRepo) EditMessage(userID, messageID, text, updatedAt string) err
 }
 
 func (r *messageRepo) DeleteMessage(userID, messageID string) error {
-	res, err := r.db.Exec(`
+	return common.RunInTransaction(r.db, func(tx *sqlx.Tx) error {
+		_, err := tx.Exec(`
+		delete from comments
+		where message_id = $1
+		  and (select user_id from messages where messages.id = $1) = $2`,
+			messageID, userID)
+		if err != nil {
+			return err
+		}
+
+		res, err := tx.Exec(`
 		delete from messages
 		where id = $1 and user_id = $2`,
-		messageID, userID)
+			messageID, userID)
+		if err != nil {
+			return err
+		}
+		rowsAffected, err := res.RowsAffected()
+		if err != nil {
+			return err
+		}
+		if rowsAffected < 1 {
+			return ErrMessageNotFound
+		}
+
+		return nil
+	})
+}
+
+func (r *messageRepo) Comments(messageIDs []string) ([]Comment, error) {
+	var comments []Comment
+	query, args, err := sqlx.In(`
+		select id, message_id, user_id, user_name, text, created_at, updated_at
+		from comments
+		where message_id in (?)
+		order by message_id, created_at`, messageIDs)
+	if err != nil {
+		return nil, err
+	}
+	query = r.db.Rebind(query)
+	err = r.db.Select(&comments, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	return comments, nil
+}
+
+func (r *messageRepo) AddComment(comment Comment) error {
+	_, err := r.db.Exec(`
+		insert into comments(id, message_id, user_id, user_name, text, created_at, updated_at)
+		select $1, $2, $3, $4, $5, $6, $7
+		where not exists(select * from comments where id = $1)`,
+		comment.ID, comment.MessageID, comment.UserID, comment.UserName, comment.Text, comment.CreatedAt, comment.UpdatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *messageRepo) EditComment(userID, commentID, text, updatedAt string) error {
+	res, err := r.db.Exec(`
+		update comments
+		set text = $1, updated_at = $2
+		where id = $3 and user_id = $4`,
+		text, updatedAt, commentID, userID)
 	if err != nil {
 		return err
 	}
@@ -97,7 +192,26 @@ func (r *messageRepo) DeleteMessage(userID, messageID string) error {
 		return err
 	}
 	if rowsAffected < 1 {
-		return ErrMessageNotFound
+		return ErrCommentNotFound
 	}
+	return nil
+}
+
+func (r *messageRepo) DeleteComment(userID, commentID string) error {
+	res, err := r.db.Exec(`
+		delete from comments
+		where id = $1 and user_id = $2`,
+		commentID, userID)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected < 1 {
+		return ErrCommentNotFound
+	}
+
 	return nil
 }
--- backend/node/services/message_service.go
+++ backend/node/services/message_service.go
@@ -76,8 +76,11 @@ func (s *messageService) Messages(ctx context.Context, r *rpc.MessageMessagesReq
 		return nil, twirp.InternalErrorWith(err)
 	}
 
+	messageIDs := make([]string, len(messages))
 	rpcMessages := make([]*rpc.Message, len(messages))
+	rpcMessageMap := make(map[string]*rpc.Message, len(messages))
 	for i, message := range messages {
+		messageIDs = append(messageIDs, message.ID)
 		rpcMessages[i] = &rpc.Message{
 			Id:        message.ID,
 			UserId:    message.UserID,
@@ -86,6 +89,23 @@ func (s *messageService) Messages(ctx context.Context, r *rpc.MessageMessagesReq
 			CreatedAt: message.CreatedAt,
 			UpdatedAt: message.UpdatedAt,
 		}
+		rpcMessageMap[message.ID] = rpcMessages[i]
+	}
+
+	comments, err := s.repos.Message.Comments(messageIDs)
+	if err != nil {
+		return nil, twirp.InternalErrorWith(err)
+	}
+	for _, comment := range comments {
+		rpcComment := &rpc.Comment{
+			Id:        comment.ID,
+			UserId:    comment.UserID,
+			UserName:  comment.UserName,
+			Text:      comment.Text,
+			CreatedAt: comment.CreatedAt,
+			UpdatedAt: comment.UpdatedAt,
+		}
+		rpcMessageMap[comment.MessageID].Comments = append(rpcMessageMap[comment.MessageID].Comments, rpcComment)
 	}
 
 	return &rpc.MessageMessagesResponse{
@@ -110,6 +130,83 @@ func (s *messageService) Delete(ctx context.Context, r *rpc.MessageDeleteRequest
 	err := s.repos.Message.DeleteMessage(user.ID, r.MessageId)
 	if err != nil {
 		if errors.Is(err, repo.ErrMessageNotFound) {
+			return nil, twirp.NotFoundError(err.Error())
+		}
+		return nil, twirp.InternalErrorWith(err)
+	}
+	return &rpc.Empty{}, nil
+}
+
+func (s *messageService) PostComment(ctx context.Context, r *rpc.MessagePostCommentRequest) (*rpc.Empty, error) {
+	user := s.getUser(ctx)
+
+	_, claims, err := s.tokenParser.Parse(r.Token, "get-messages")
+	if err != nil {
+		if errors.Is(err, token.ErrInvalidToken) {
+			return nil, twirp.NewError(twirp.InvalidArgument, "invalid token")
+		}
+		return nil, twirp.InternalErrorWith(err)
+	}
+
+	if user.ID != claims["id"].(string) || s.externalAddress != claims["node"].(string) {
+		return nil, twirp.NewError(twirp.InvalidArgument, "invalid token")
+	}
+
+	message, err := s.repos.Message.Message(r.MessageId)
+	if err != nil {
+		if errors.Is(err, repo.ErrMessageNotFound) {
+			return nil, twirp.NotFoundError(err.Error())
+		}
+		return nil, twirp.InternalErrorWith(err)
+	}
+
+	rawUserIDs := claims["users"].([]interface{})
+	found := false
+	for _, rawUserID := range rawUserIDs {
+		userID := rawUserID.(string)
+		if userID == message.UserID {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return nil, twirp.NotFoundError(repo.ErrMessageNotFound.Error())
+	}
+
+	err = s.repos.Message.AddComment(repo.Comment{
+		ID:        r.Comment.Id,
+		MessageID: r.MessageId,
+		UserID:    claims["id"].(string),
+		UserName:  claims["name"].(string),
+		Text:      r.Comment.Text,
+		CreatedAt: r.Comment.CreatedAt,
+		UpdatedAt: r.Comment.UpdatedAt,
+	})
+	if err != nil {
+		return nil, twirp.InternalErrorWith(err)
+	}
+
+	return &rpc.Empty{}, nil
+}
+
+func (s *messageService) EditComment(ctx context.Context, r *rpc.MessageEditCommentRequest) (*rpc.Empty, error) {
+	user := s.getUser(ctx)
+	err := s.repos.Message.EditComment(user.ID, r.CommentId, r.Text, r.UpdatedAt)
+	if err != nil {
+		if errors.Is(err, repo.ErrCommentNotFound) {
+			return nil, twirp.NotFoundError(err.Error())
+		}
+		return nil, twirp.InternalErrorWith(err)
+	}
+	return &rpc.Empty{}, nil
+}
+
+func (s *messageService) DeleteComment(ctx context.Context, r *rpc.MessageDeleteCommentRequest) (*rpc.Empty, error) {
+	user := s.getUser(ctx)
+	err := s.repos.Message.DeleteComment(user.ID, r.CommentId)
+	if err != nil {
+		if errors.Is(err, repo.ErrCommentNotFound) {
 			return nil, twirp.NotFoundError(err.Error())
 		}
 		return nil, twirp.InternalErrorWith(err)
