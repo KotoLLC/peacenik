6424030fef1d82c6551e1d9445525ee708712e16
dimakrsna <dimakrsna@gmail.com>
2020-07-04 13:18:58 +0300

    Logout reworked to icon button

--- frontend/src/view/shared/TopBar.tsx
+++ frontend/src/view/shared/TopBar.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import { ButtonStyled } from './styles'
+import { TooltipStyle, IconButtonStyled } from './styles'
 import { connect } from 'react-redux'
 import Actions from '@store/actions'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 
 type DispatchProps = Pick<Props, 'onLogout'>
 const mapDispatchToProps = (dispatch): DispatchProps => ({
@@ -16,13 +17,17 @@ interface Props {
 }
 
 const TopBar: React.SFC<Props> = React.memo((props) => {
-  const {onLogout} = props
+  const { onLogout } = props
 
   return (
     <AppBar position="fixed" color="primary">
       <Toolbar>
         <Typography variant="h6">Koto</Typography>
-        <ButtonStyled color="inherit" onClick={onLogout}>Logout</ButtonStyled>
+        <TooltipStyle title={`Logout`}>
+          <IconButtonStyled onClick={onLogout}>
+            <ExitToAppIcon />
+          </IconButtonStyled>
+        </TooltipStyle>
       </Toolbar>
     </AppBar>
   )
--- frontend/src/view/shared/styles.ts
+++ frontend/src/view/shared/styles.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components'
 import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import Tooltip from '@material-ui/core/Tooltip'
 
-export const ButtonStyled = styled(Button)`
+export const TooltipStyle = styled(Tooltip)`
   && {
     margin-left: auto;
+  }
+`
+
+export const IconButtonStyled = styled(IconButton)`
+  && {
+    color: #fff;
   }
 `
