9d79e90686bd9cc65654c0cd9a14193234b18431
dimakrsna <dimakrsna@gmail.com>
2020-07-09 16:06:16 +0300

    #49 is admin check

--- frontend/src/sagas/index.ts
+++ frontend/src/sagas/index.ts
@@ -1,8 +1,8 @@
 import { all, takeEvery } from 'redux-saga/effects'
-
 import { Types as AuthorizationTypes } from '@store/authorization/actions'
 import { Types as FriendTypes } from '@store/friends/actions'
 import { Types as NodeTypes } from '@store/nodes/actions'
+import { Types as ProfileTypes } from '@store/profile/actions'
 
 import { watchlogin, watchlogout } from './authorization'
 import { 
@@ -13,13 +13,13 @@ import {
     watchAcceptInvitation,
     watchRejectInvitation,
 } from './friends'
-
 import { 
     watchNodeCreate, 
     watchGetNodes, 
     watchApproveNode,
     watchRemoveNode,
 } from './nodes'
+import { watchGetProfile } from './profile'
 
 export function* rootSaga() {
     yield all([
@@ -35,5 +35,6 @@ export function* rootSaga() {
         takeEvery(NodeTypes.GET_NODES_REQUEST, watchGetNodes),
         takeEvery(NodeTypes.APPROVE_NODE_REQUEST, watchApproveNode),
         takeEvery(NodeTypes.REMOVE_NODE_REQUEST, watchRemoveNode),
+        takeEvery(ProfileTypes.GET_PROFILE_REQUEST, watchGetProfile),
     ])
 }
