b664ab8d245f05a35884c70da87d89d05b9c71c2
Matthew Reider <matthew.reider@dynatrace.com>
2020-07-14 20:16:55 +0200

    code of conduct

--- frontend/src/view/pages/DocsPages/CodeOfConductPage.tsx
+++ frontend/src/view/pages/DocsPages/CodeOfConductPage.tsx
@@ -3,7 +3,52 @@ import Typography from '@material-ui/core/Typography'
 
 export const CodeOfConductPage = React.memo(() => (
   <>
-    <Typography variant="h4" component="h4" gutterBottom>Code Of Conduct</Typography>
-    <Typography variant="body1" gutterBottom>Description is comming soon</Typography>
+    <Typography variant="h3" component="h3" gutterBottom>
+      Code Of Conduct
+      </Typography>
+    <Typography variant="h5" component="h5" gutterBottom>
+      No bullying
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+    Koto is for sharing and belonging, not for attacking marginalized or vulnerable groups of people. 
+    Everyone has a right to use Koto free of harassment, bullying, and threats of violence. Users that
+    incite violence or that promote hate based on identity or vulnerability will be banned.
+    </Typography>
+    <Typography variant="h5" component="h5" gutterBottom>
+      No inappropriate material
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+    Do not post or encourage the posting of graphic, sexually-explicit, or offensive content.
+    </Typography>
+    <Typography variant="h5" component="h5" gutterBottom>
+      No hate speech
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+    People that incite violence or that promote hate based on identity or vulnerability will be banned. Marginalized or vulnerable groups include those based on actual and perceived race, color, religion, national origin, ethnicity, immigration status, gender, gender identity, sexual orientation, pregnancy, or disability. These include victims of a major violent event and their families. While the rule on hate protects such groups, it does not protect those who promote attacks of hate or who try to hide their hate in bad faith claims of discrimination.
+    </Typography>
+    <Typography variant="h5" component="h5" gutterBottom>
+      No threats
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+    Do not threaten, advocate for, celebrate, or express extreme indifference towards any kind of action that results in or could result in death or harm (physical or otherwise). This rule applies to everyone. Users who engage in this behavior may be banned.
+    </Typography>
+    <Typography variant="h5" component="h5" gutterBottom>
+      No trolling
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+    Trolling of any kind is not allowed. Trolling is the act of commenting or submitting links with the intent to shock, anger, or sow discord without good faith. Good faith is sincere intention to be fair, open, and honest, regardless of the outcome of the interaction
+    </Typography>
+    <Typography variant="h5" component="h5" gutterBottom>
+      No baiting
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+    If you disagree with someone, don't try to bait them into behavior that they would regret. If you think you are the victim of flaming or baiting, report them and  move on. Don't engage in personal attacks because someone is mistreating you.
+    </Typography>
+    <Typography variant="h5" component="h5" gutterBottom>
+      No personal attacks
+    </Typography>
+    <Typography variant="body1" gutterBottom>
+    It doesn't matter how "well-written" the comment, if your insult is sarcastic, "creative" or absurd, personal attacks are always against our rules. Name calling, ad-hominem, demeaning, inflammatory, or other uncivil comments directed at other users are not allowed. Users who break this rule may have their comment(s) removed and be warned and/or banned. you.
+    </Typography>
   </>
 ))
