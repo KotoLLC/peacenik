679216bb7d2c1cc07da36bab5e57a723d2c8cdf9
Matthew Reider <matthew.reider@dynatrace.com>
2020-08-18 16:39:51 +0200

    docs and stuff

--- frontend/src/view/pages/AboutUsPage/index.tsx
+++ frontend/src/view/pages/AboutUsPage/index.tsx
@@ -6,9 +6,7 @@ import { WithTopBar } from '@view/shared/WithTopBar'
 import { ContainerStyled } from './styles'
 import ReactMarkdown from 'react-markdown'
 import Button from '@material-ui/core/Button'
-import milhousePicture from './../../../assets/images/milhouse.gif'
-import waitPicture from './../../../assets/images/oneMore.gif'
-import dothisPicture from './../../../assets/images/dothis.gif'
+import nothingPicture from './../../../assets/images/nothing.gif'
 
 interface Props extends RouteComponentProps {
   onSetAboutUsViewed: () => void
@@ -19,53 +17,57 @@ export const AboutUsSlider: React.SFC<Props> = (props) => {
   const onGoToHubs = () => {
     props.history.push('/hubs/create')
     props.onSetAboutUsViewed()
-    localStorage.setItem('kotoIsAboutUsViewed', 'true')
+    //localStorage.setItem('kotoIsAboutUsViewed', 'true')
   }
 
   const onGoToInvites = () => {
     props.history.push('/friends/invitations')
     props.onSetAboutUsViewed()
-    localStorage.setItem('kotoIsAboutUsViewed', 'true')
+    //localStorage.setItem('kotoIsAboutUsViewed', 'true')
   }
 
   return (
     <WithTopBar>
       <ContainerStyled maxWidth="md">
         <ReactMarkdown>{`
-  # Welcome to Koto
-  Like most social networks, koto starts with friends. And from the look of it, you don't have any.
+  # Getting started
   
-  ![](${milhousePicture})
+  ## You need two things:
 
-  # Making friends
-  Making friends is easy.
-  Just go to the friends page and press the add friends button.
+  1. Friends
+  2. Message hubs
+
+  ![](${nothingPicture})
+
+  ##  Making friends
+
+  Making friends is easy. Just visit the friends page and invite someone.
+
+  ## Message hubs
+
+  Message hubs store messages, photos, and videos. Without a hub, you can't
+  post a message.
+
+  Every group of friends needs a hub. One person becomes a hub runner, and everyone
+  else uses that person's hub.
+
+  ## Summer / Fall 2020
+
+  As we're just getting started, everyone can use my hub, by sending an invite
+  to mreider@gmail.com. Or, if you'd like toexperiment, feel free to start your own.
+
   `}
         </ReactMarkdown>
         <Button
           variant="contained"
           color="primary"
           onClick={onGoToInvites}>
-          Invite a friend
-          </Button>
-        <ReactMarkdown>{`
-  Oh. Wait. There's one more thing
-  
-  ![](${waitPicture})
-
-  # Message hubs
-  So. One last thing.
-
-  One of your friends needs to run a message hub. Or, if you know about software, you can run it for them. Without a message hub, they can't post messages.
-
-  ![](${dothisPicture})
-  `}
-        </ReactMarkdown>
-        <Button
+          Invite friends
+          </Button> <Button
           variant="contained"
           color="primary"
           onClick={onGoToHubs}>
-          Create a message hub
+          Create a hub
           </Button>
       </ContainerStyled>
     </WithTopBar>
