62657d0283bba33885fa739bc6900d708ce8adb3
Matthew Reider <matthew.reider@dynatrace.com>
2020-11-27 15:54:31 +0100

    Revert "#138 Youtube links"
    This reverts commit d867642d2f78886be92ff4af2fdc76449ffe0996.

--- frontend/src/view/pages/MessagesPage/Message.tsx
+++ frontend/src/view/pages/MessagesPage/Message.tsx
@@ -55,7 +55,7 @@ import {
 } from './styles'
 
 function LinkRenderer(props) {
-  return <a href={props.href} rel="noopener noreferrer" target="_blank">{props.children}</a>
+  return <a href={props.href} target="_blank">{props.children}</a>
 }
 
 interface Props extends ApiTypes.Messages.Message {
