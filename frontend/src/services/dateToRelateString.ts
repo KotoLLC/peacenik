c0977aab027b94c3118afe33511641108d3a050e
Cardoso-topdev <Oscar@cardoso-devstar.com>
2021-04-12 03:14:17 -0600

    add file

---
+++ frontend/src/services/dateToRelateString.ts
@@ -0,0 +1,22 @@
+export const dateToRelateString = (d: string | null) => {
+  if(!d) return;
+  const current = new Date();
+  const oldDate = new Date(d);
+  const diffSecond = (current.getTime() - oldDate.getTime()) / 1000;
+
+  if(diffSecond < 60) {
+    return 'just ago'
+  } else if(diffSecond < 600) {
+    return 'Less in 10 min'
+  } else if(diffSecond < 1800) {
+    return `${Math.ceil(diffSecond/60)} mins ago`
+  } else if(diffSecond < 12 * 3600) {
+    return `${Math.ceil(diffSecond/3600)} hours ago`
+  } else {
+    const y = oldDate.getFullYear();
+    const m = oldDate.getMonth() + 1;
+    const d = oldDate.getDate();
+    return `${y}-${m}-${d}`
+  }
+  
+}
