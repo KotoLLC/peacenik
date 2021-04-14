8053579bf1bdc33a45b136da76e86c84a7970712
Andrey Sokolov <falconandy@yandex.ru>
2021-04-14 14:21:39 +0400

    Added more details to "invalid token" errors

--- backend/token/token_parser.go
+++ backend/token/token_parser.go
@@ -2,13 +2,10 @@ package token
 
 import (
 	"crypto/rsa"
+	"errors"
+	"fmt"
 
-	"github.com/ansel1/merry"
 	"github.com/dgrijalva/jwt-go"
-)
-
-var (
-	ErrInvalidToken = merry.New("invalid token")
 )
 
 type Parser interface {
@@ -28,21 +25,21 @@ func NewParser(getPublicKey func() *rsa.PublicKey) Parser {
 func (p *parser) Parse(rawToken string, scope string) (token *jwt.Token, claims jwt.MapClaims, err error) {
 	jwtToken, err := jwt.Parse(rawToken, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, ErrInvalidToken.Here()
+			return nil, errors.New("invalid token: signing method")
 		}
 		return p.getPublicKey(), nil
 	})
 	if err != nil {
-		return nil, nil, ErrInvalidToken.Here()
+		return nil, nil, fmt.Errorf("invalid token: %w", err)
 	}
 
 	claims = jwtToken.Claims.(jwt.MapClaims)
 	if !jwtToken.Valid {
-		return jwtToken, claims, ErrInvalidToken.Here()
+		return jwtToken, claims, errors.New("invalid token: not valid")
 	}
 
 	if scope != claims["scope"].(string) {
-		return jwtToken, claims, ErrInvalidToken.Here()
+		return jwtToken, claims, fmt.Errorf("invalid token (expected scope %s, was %s)", scope, claims["scope"].(string))
 	}
 	return jwtToken, claims, nil
 }
