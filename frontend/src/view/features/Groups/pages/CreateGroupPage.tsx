0b92fa3aaafe8ac3495f2386c27d638ecf8289d4
Matt Reider <mreider@gmail.com>
2021-04-18 21:42:43 +0200

    Update CreateGroupPage.tsx

--- frontend/src/view/features/Groups/pages/CreateGroupPage.tsx
+++ frontend/src/view/features/Groups/pages/CreateGroupPage.tsx
@@ -341,8 +341,8 @@ const CreateGroupPage: React.FC<Props> = (props) => {
       </EmptyGroupsIconWrapper>
       <EmptyGroupsTextWrapper>
         <EmptyGroupsText>
-          Sorry! The hub you're using doesn't support groups. <EmptyGroupsTextLink to="/settings/hub">
-            Visit the hub page</EmptyGroupsTextLink> to create a group-enabled hub.
+          Sorry! You can only create groups on your own message hub. <EmptyGroupsTextLink to="/settings/hub">
+            Visit the hub page</EmptyGroupsTextLink> to create a hub and host your own group.
       </EmptyGroupsText>
       </EmptyGroupsTextWrapper>
     </EmptyScrenWrapper>
